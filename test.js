f6981e98e551da7669d98dfa5343f7657a9cb668
James Cruise <cruise12343@gmail.com>
2023-10-13 17:37:18 +0530

    test3 added

--- test.js
+++ test.js
@@ -120,14 +120,15 @@ function generateFloats ({ serverSeed, clientSeed, nonce, cursor, count }) {
 
   // Populate bytes array with sets of 4 from RNG output
   while (bytes.length < count * 4) {
-    bytes.push(rng.next().value);console.log(bytes);
+    bytes.push(rng.next().value);
   }
-
+    console.log(bytes);
   // Return bytes as floats using lodash reduce function
   return _.chunk(bytes, 4).map(bytesChunk =>
     bytesChunk.reduce((result, value, i) => {
       const divider = 256 ** (i + 1);
       const partialResult = value / divider;
+	  console.log(`\nindex:${i}\nvalue:${value}\ndivider:${divider}\npartialResult:${partialResult}\nresult:${result}\n------------`);
       return result + partialResult;
     }, 0)
   );
@@ -156,3 +157,27 @@ let res = generateFloats(obj);
 
 console.log(res);
 console.log(((res[0]*10001)/100).toFixed(2));
+
+
+//console.log('------------\ndivider:${divider}\npartialResult:${partialResult}\nvalue:${value}\nresult:${result}\n-----------');
+
+/*
+let arr=[63,192,195,26];
+
+function fun(result,value,i){
+    const divider = 256 ** (i + 1);
+      const partialResult = value / divider;
+	  console.log(`-----------\nindex:${i}\nvalue:${value}\ndivider:${divider}\npartialResult:${partialResult}\nresult:${result}\n-----------`);
+      return result + partialResult;
+}
+
+arr.reduce(fun,0);
+
+*/
+
+
+
+/*
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
